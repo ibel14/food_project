e1762e3ee10e8e9031931aa4dde68a61b206bc36
ponaehali <iambelousov@gmail.com>
2020-10-01 22:42:16 +0200

    #38 repeat

--- js/script.js
+++ js/script.js
@@ -1,41 +1,21 @@
 "use strict";
 
 window.addEventListener('DOMContentLoaded', () => {
+                                    // Создал переменные под каждую вкладку
+    const tabs = document.querySelectorAll('.tabheader__item'), // Каждая вкладка
+          tabsContent = document.querySelectorAll('.tabcontent'), // Описание вкладок
+          tabsParent = document.querySelector('.tabheader__items'); // Отвечает за весь блок вкладок
 
-    const tabs = document.querySelectorAll('.tabheader__item'),
-          tabsContent = document.querySelectorAll('.tabcontent'),
-          tabsParent = document.querySelector('.tabheader__items');
-
-    function hideTabContent() {
-        tabsContent.forEach(item => {
-            item.classList.add('hide');
-            item.classList.remove('show', 'fade');
-        });
+    
+    function hideTabContent() { // Скрываем ненужные вкладки
+        tabsContent.forEach(item => { // Перебираем каждый контент отдельно через форич
+            item.style.display = 'none'; // Скрыли весь контент которые есть на сайте
+        }); // Эта функция занимается только скрытием вкладок (табов)
 
         tabs.forEach(item => {
             item.classList.remove('tabheader__item_active');
         });
     }
 
-    function showTabContent(i = 0) {
-        tabsContent[i].classList.add('show', 'fade');
-        tabsContent[i].classList.remove('hide');
-        tabs[i].classList.add('tabheader__item_active');
-    }
-    
-    hideTabContent();
-    showTabContent();
 
-    tabsParent.addEventListener('click', (event) => {
-        const target = event.target;
-
-        if (target && target.classList.contains('tabheader__item')) {
-            tabs.forEach((item, i) => {
-                if (target == item) {
-                    hideTabContent();
-                    showTabContent(i);
-                }
-            });
-        }
-    });
 });
