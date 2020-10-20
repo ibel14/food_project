af3b321b898f333be01841e0aff02f5cd6ee6435
ponaehali <iambelousov@gmail.com>
2020-10-20 22:46:44 +0200

    #49 rest fixed

--- js/script.js
+++ js/script.js
@@ -151,12 +151,13 @@ window.addEventListener('DOMContentLoaded', () => {
     // Используем классы для карточек
 
     class MenuCard {
-        constructor(src, alt, title, descr, price, parentSelector) {
+        constructor(src, alt, title, descr, price, parentSelector, ...classes) {
             this.src = src;
             this.alt = alt;
             this.title = title;
             this.descr = descr;
             this.price = price;
+            this.classes = classes;
             this.parent = document.querySelector(parentSelector);
             this.transfer = 70;
             this.changeToRub();
@@ -164,7 +165,6 @@ window.addEventListener('DOMContentLoaded', () => {
 
         changeToRub() {
             this.price = this.price * this.transfer;
-        }
 
         render() {
             const element = document.createElement('div');
@@ -175,7 +175,6 @@ window.addEventListener('DOMContentLoaded', () => {
             } else {
                 this.classes.forEach(className => element.classList.add(className));
             }
-        }
 
         element.innerHTML = `
         <img src=${this.src} alt=${this.alt}>
@@ -185,14 +184,14 @@ window.addEventListener('DOMContentLoaded', () => {
         <div class="menu__item-price">
             <div class="menu__item-cost">Цена:</div>
             <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
-        </div>
     `;
     this.parent.append(element);
+            
 }
 
 new MenuCard(
-    "img/tabs/vegy.jpg",
-    "vegy",
+    'img/tabs/vegy.jpg',
+    'vegy',
     'Меню "Фитнес"',
     'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
     9,
@@ -205,7 +204,8 @@ new MenuCard(
     'Меню "Постное"',
     'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
     14,
-    ".menu .container"
+    ".menu .container",
+    'menu__item'
 ).render();
 
 new MenuCard(
@@ -214,7 +214,8 @@ new MenuCard(
     'Меню “Премиум”',
     'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
     21,
-    ".menu .container"
+    ".menu .container",
+    'menu__item'
 ).render();
 
 });
