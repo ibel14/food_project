85a92e929ceed98b97aeea6f76194cb05b10747d
ponaehali <iambelousov@gmail.com>
2020-11-07 23:19:05 +0200

    #58 npm

--- js/script.js
+++ js/script.js
@@ -294,4 +294,9 @@ new MenuCard(
             closeModal();
         }, 4000);
     }
+
+    fetch('http://localhost:3000/menu')
+    .then(data => data.json())
+    .then(res => console.log(res));
+
 });
